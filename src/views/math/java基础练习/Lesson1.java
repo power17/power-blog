f1d024dc081988c2c587877b9336a6c138c5c28f
power <1410837981@qq.com>
2023-09-17 22:47:12 +0800

    feat: 单循环冒泡

--- src/views/math/java基础练习/Lesson1.java
+++ src/views/math/java基础练习/Lesson1.java
@@ -1,26 +1,33 @@
-package views.math.java基础练习;
+package java基础练习;
 
 import java.util.Arrays;
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class Lesson1 {
+
   public static void main(String[] args) {
-    // Scanner sc = new Scanner(System.in);
-    // System.out.println("input you last score");
-    // String score = sc.nextLine();
-    // System.out.println(score);
-    int[] ns = { 28, 12, 89, 73, 65, 18, 96, 50, 8, 36 };
+    int[] nums = { 28, 12, 89, 73, 65, 18, 96, 50, 8, 36 };
     // 冒泡排序
-    for (int i = 0; i < ns.length - 1; i++) {
-      for (int j = 0; j < ns.length - 1 - i; j++) {
-        if (ns[j] < ns[j + 1]) {
-          int tem = ns[j];
-          ns[j] = ns[j + 1];
-          ns[j + 1] = tem;
-        }
+    int end = nums.length - 1;
+
+    for (int j = 0; j <= end; j++) {
+
+      if (j == end) {
+        end = end - 1;
+        j = 0;
+      }
+      if (nums[j] > nums[j + 1]) {
+        int tem = nums[j];
+        nums[j] = nums[j + 1];
+        nums[j + 1] = tem;
       }
     }
-    System.out.println(Arrays.toString(ns));
+    return nums;
+
+    // for (int i = 0; i < ns.length - 1; i++) {
+
+    // }
+    System.out.println(Arrays.toString(nums));
 
   }
 }
