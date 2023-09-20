886816f8318222b2af20a4b721e11db096914a7c
power <1410837981@qq.com>
2023-09-20 17:16:26 +0800

    feat: dev

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,32 +1,36 @@
 import { defineConfig } from 'vitepress'
-
 // https://vitepress.dev/reference/site-config
-export default defineConfig({
-  title: "power的博客",
-  description: "记录技术的点点滴滴",
-  srcDir: 'src',
-  lastUpdated: true,
-  base: "",
+export default defineConfig((config) => {
+  let base = config.mode === 'development' ? '/' : '/power-learn' 
+
+  return {
+    title: "power的博客",
+    description: "记录技术的点点滴滴",
+    srcDir: 'src',
+    lastUpdated: true,
+    base: base,
+    
+    themeConfig: {
+     
+      // https://vitepress.dev/reference/default-theme-config
+      nav: [
+        { text: '首页', link: '/' },
+        { text: '算法', link: '/algorithm/Array' }
+      ],
   
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: '首页', link: '/' },
-      { text: '算法', link: '/algorithm/Array' }
-    ],
-
-    sidebar: [
-      {
-        text: '算法',
-        items: [
-          { text: '数组', link: '/algorithm/Array' },
-          { text: '链表', link: '/algorithm/LinkedList' }
-        ]
-      }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/power17/power-learn' }
-    ]
+      sidebar: [
+        {
+          text: '算法',
+          items: [
+            { text: '数组', link: '/algorithm/Array' },
+            { text: '链表', link: '/algorithm/LinkedList' }
+          ]
+        }
+      ],
+  
+      socialLinks: [
+        { icon: 'github', link: 'https://github.com/power17/power-learn' }
+      ]
+    }
   }
 })
