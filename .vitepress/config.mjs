9e393534daf839dff89a8657e8891c336630b6e7
power <1410837981@qq.com>
2024-03-09 12:31:46 +0800

    feat: add system 安装

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,91 +1,84 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from 'vitepress';
 export default defineConfig((config) => {
-  let base = process.env.BUILD_TYPE === 'git' ? '/power-blog' : '' 
+  let base = process.env.BUILD_TYPE === 'git' ? '/power-blog' : '';
 
   return {
-    title: "疯世权王",
-    description: "记录技术的点点滴滴",
+    title: '疯世权王',
+    description: '记录技术的点点滴滴',
     head: [
       // 图标配置
-      ['link', { rel: 'icon',type:"image/x-icon", href: base + '/logo.svg' }],
-   
+      ['link', { rel: 'icon', type: 'image/x-icon', href: base + '/logo.svg' }],
     ],
     srcDir: 'src',
-  
+
     lastUpdated: true,
     base: base,
     themeConfig: {
       // externalLinkIcon: true,
       logo: '/logo.png',
       search: {
-        provider: 'local'
+        provider: 'local',
       },
       nav: [
-        
         { text: '首页', link: '/' },
+        { text: '博文', link: '/blog/System.md' },
         { text: '前端', link: '/frontend/Vscode' },
         { text: '后端', link: '/backend/node' },
-        { text: '算法', link: '/algorithm/Array'},
-        { text: '数据库', link: '/databases/Mysql'},
+        { text: '算法', link: '/algorithm/Array' },
+        { text: '数据库', link: '/databases/Mysql' },
         { text: '工程', link: '/engineering/Git' },
-        { text: 'mac', link: '/mac/macEvironment'},
-        { text: 'window', link: '/window/Command.md'},
-        { text: '收藏', link: '/book/Recommend.md'},
-        { text: '问题（坑）', link: '/pit/Problem.md'},
-       
+        { text: 'mac', link: '/mac/macEvironment' },
+        { text: 'window', link: '/window/Command.md' },
+        { text: '收藏', link: '/book/Recommend.md' },
+        { text: '问题（坑）', link: '/pit/Problem.md' },
       ],
-       sidebar: {
+      sidebar: {
         '/algorithm/': [
           {
             text: '数据结构',
             items: [
               { text: '数组', link: '/algorithm/Array' },
-              { text: '链表', link: '/algorithm/LinkedList' }
-            ]
+              { text: '链表', link: '/algorithm/LinkedList' },
+            ],
           },
           {
             text: '算法',
-            items: [
-              { text: '排序', link: '/algorithm/Sort'},
-            ]
-          }
+            items: [{ text: '排序', link: '/algorithm/Sort' }],
+          },
         ],
         '/frontend/': [
           {
             text: '源码系列',
-            items: [
-              { text: 'vscode调试', link: '/frontend/Vscode' },
-            ]
+            items: [{ text: 'vscode调试', link: '/frontend/Vscode' }],
           },
           {
             text: 'css',
-            items: [
-              { text: '重学css', link: '/frontend/Css' },
-            ]
-          }
-        ], 
+            items: [{ text: '重学css', link: '/frontend/Css' }],
+          },
+        ],
+        '/blog/': [
+          {
+            text: '计算级',
+            items: [{ text: 'linux-ubunt22安装', link: '/blog/System' }],
+          },
+        ],
         '/backend/': [
           {
             text: 'node系列',
             items: [
               { text: '开发node应用', link: '/backend/Node' },
               { text: 'Nest精髓', link: '/backend/Nest' },
-            ]
+            ],
           },
-          {
-            
-          }
-        ],  
+          {},
+        ],
         '/databases/': [
           {
             text: '数据库',
-            items: [
-              { text: 'mysql基本操作', link: '/databases/Mysql' },
-             
-            ]
+            items: [{ text: 'mysql基本操作', link: '/databases/Mysql' }],
           },
-         ],
-          '/engineering/': [
+        ],
+        '/engineering/': [
           {
             text: '工程',
             items: [
@@ -93,12 +86,10 @@ export default defineConfig((config) => {
               { text: 'esbuild基本使用', link: '/engineering/esbuild' },
 
               { text: 'rollup使用', link: '/engineering/rollup' },
-              { text: 'webpack基本使用', link: '/engineering/Webpack' },  
+              { text: 'webpack基本使用', link: '/engineering/Webpack' },
               { text: 'vite基本使用', link: '/engineering/Engineering' },
               { text: 'vite插件', link: '/engineering/VitePlugin' },
-          
-              
-            ]
+            ],
           },
         ],
         '/mac/': [
@@ -108,51 +99,38 @@ export default defineConfig((config) => {
               { text: 'mac配置环境', link: '/mac/macEvironment' },
               { text: 'VscodeDebugger', link: '/mac/VscodeDebugger' },
               { text: 'linux命令', link: '/mac/linuxCommand' },
-             
-            ]
+            ],
           },
-         ],
-         '/window/': [
+        ],
+        '/window/': [
           {
             text: 'window系统',
-            items: [
-              { text: 'window命令行', link: '/window/Command' },
-             
-             
-            ]
+            items: [{ text: 'window命令行', link: '/window/Command' }],
           },
-         ],
+        ],
         // book/Recommend.md
-         '/book/': [
-            {
+        '/book/': [
+          {
             text: '书籍推荐',
             items: [
               { text: '计算机', link: '/book/Recommend' },
-              { text: '收藏夹', link: '/book/Favorites' },            
-              { text: '谷歌搜索技术', link: '/book/Search' },            
-             
-            ]
+              { text: '收藏夹', link: '/book/Favorites' },
+              { text: '谷歌搜索技术', link: '/book/Search' },
+            ],
           },
-         ],
-            '/pit/': [
-            {
+        ],
+        '/pit/': [
+          {
             text: '问题记录',
             items: [
               { text: 'window', link: '/pit/Problem' },
               { text: '前端', link: '/pit/Frontend' },
-      
-             
-             
-            ]
+            ],
           },
-        ]
-       }, 
-  
-      
-  
-      socialLinks: [
-        { icon: 'github', link: 'https://github.com/power17/power-learn' }
-      ]
-    }
-  }
-})
+        ],
+      },
+
+      socialLinks: [{ icon: 'github', link: 'https://github.com/power17/power-learn' }],
+    },
+  };
+});
---
+++ src/blog/System.md
@@ -0,0 +1,24 @@
+---
+title: linux系统的安装
+author: power
+description: 'ubuntu22安装'
+date: 2024-03-09 12:29:08
+link: ./blog/System
+tags:
+  - system
+---
+
+# {{$frontmatter.title}}
+
+- {{$frontmatter.description}}
+
+- [参考链接](https://blog.csdn.net/weixin_70137390/article/details/124724957)
+
+### 1、下载系统镜像文件
+
+- [Ubuntu 22.04.4 LTS](https://ubuntu.com/download/desktop)
+- [烧录工具：rufus](https://rufus.ie/zh/)
+
+### 2、安装 ubuntu
+
+- 重启电脑 按 del 进入 bios，u 盘启动
